e8d239c98b95fce19ad6c386103c66d95bb7e866
prophet <wangjiawei1312@gmail.com>
2024-02-11 09:32:09 +0800

    merge

--- src/Core/Scene.ts
+++ src/Core/Scene.ts
@@ -120,11 +120,11 @@ class Scene {
         samples: this.msaaSamples,
       },
       {
-        format: this.gl.RGBA8, // internal format must be  gl.RGBA8 not gl.RGBA not same
+        // format: this.gl.RGBA8, // internal format must be  gl.RGBA8 not gl.RGBA not same
         type: this.gl.UNSIGNED_BYTE,
         minMag: this.gl.NEAREST,
         wrap: this.gl.CLAMP_TO_EDGE,
-        target: this.gl.RENDERBUFFER,
+        // target: this.gl.RENDERBUFFER,
         width: this.gl.drawingBufferWidth,
         height: this.gl.drawingBufferHeight,
         samples: this.msaaSamples,
@@ -174,22 +174,33 @@ class Scene {
 
       }
 
-    }
-    for(let i=0; i<msaaFbo.attachments.length-1; i++){
+      for(let i=0; i<msaaFbo.attachments.length-1; i++){
 
-      gl.bindFramebuffer(gl.READ_FRAMEBUFFER, msaaFbo.framebuffer);
-      gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, colorFbo.framebuffer);
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, msaaFbo.framebuffer);
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, colorFbo.framebuffer);
 
-      if (isMultipleRenderTargerts ) {
-        gl.framebufferRenderbuffer( gl.READ_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, msaaFbo.attachments[i] );
-        gl.framebufferTexture2D( gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, colorFbo.attachments[i] , 0 );
+        if (isMultipleRenderTargerts ) {
+          gl.framebufferRenderbuffer( gl.READ_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.RENDERBUFFER, msaaFbo.attachments[i] );
+          gl.framebufferTexture2D( gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, colorFbo.attachments[i] , 0 );
+        }
+        gl.blitFramebuffer(
+          0, 0, canvas.width, canvas.height,
+          0, 0, canvas.width, canvas.height,
+          gl.COLOR_BUFFER_BIT,
+          gl.NEAREST
+        );
       }
-      gl.blitFramebuffer(
-        0, 0, canvas.width, canvas.height,
-        0, 0, canvas.width, canvas.height,
-        gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT,
-        gl.NEAREST
-      );
+    }else{
+
+        gl.bindFramebuffer(gl.READ_FRAMEBUFFER, msaaFbo.framebuffer);
+        gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, colorFbo.framebuffer);
+        gl.blitFramebuffer(
+          0, 0, canvas.width, canvas.height,
+          0, 0, canvas.width, canvas.height,
+          gl.COLOR_BUFFER_BIT,
+          gl.NEAREST
+        );
+
     }
     gl.bindFramebuffer(gl.READ_FRAMEBUFFER, null);
     gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
@@ -222,13 +233,52 @@ class Scene {
 
   }
 
+<<<<<<< Updated upstream
 	render(screenFbo?: twgl.FramebufferInfo) {
+=======
+  fboDepth2fbo(sourceFramebuffer: WebGLFramebuffer, sourceDepthTexture: WebGLTexture, targetFramebuffer: WebGLFramebuffer, targetDepthTexture: WebGLTexture){
+    const gl = this.gl;
+    // 首先，我们需要将sourceFramebuffer绑定到当前的Framebuffer
+    gl.bindFramebuffer(gl.FRAMEBUFFER, sourceFramebuffer);
+
+    // 然后，我们需要将sourceDepthTexture绑定到当前的纹理
+    gl.bindTexture(gl.TEXTURE_2D, sourceDepthTexture);
+
+    // 接下来，我们需要将当前的Framebuffer的颜色缓冲区设置为不可读可写
+    gl.colorMask(false, false, false, false);
+    // 同时，我们需要将当前的Framebuffer的深度缓冲区设置为可读可写
+    gl.depthMask(true);
+
+    // 然后，我们需要将targetFramebuffer绑定到当前的Framebuffer
+    gl.bindFramebuffer(gl.FRAMEBUFFER, targetFramebuffer);
+
+    // 接下来，我们需要将targetDepthTexture绑定到当前的纹理
+    gl.bindTexture(gl.TEXTURE_2D, targetDepthTexture);
+
+    // 我们需要将当前的Framebuffer的颜色缓冲区设置为不可读可写
+    gl.colorMask(false, false, false, false);
+    // 同时，我们需要将当前的Framebuffer的深度缓冲区设置为可读可写
+    gl.depthMask(true);
+
+
+    const width = this.gl.drawingBufferWidth;
+    const height = this.gl.drawingBufferHeight;
+
+    // 接下来，我们需要使用gl.copyTexImage2D来将sourceDepthTexture的内容复制到targetDepthTexture
+    gl.copyTexImage2D(gl.TEXTURE_2D, 0, gl.DEPTH_COMPONENT, 0, 0, width, height, 0);
+
+    // 最后，我们需要将颜色和深度的mask设置回来
+    gl.colorMask(true, true, true, true);
+    gl.depthMask(false);
+  }
+
+	render(screenFbo: twgl.FramebufferInfo) {
     this.updateAndClearFrameBuffer();
 
     const gl = this.gl;
 
     const currentFbo = gl.getParameter(gl.FRAMEBUFFER_BINDING);
-    // console.log(' currentFbo ', currentFbo);
+    const currentFbo = screenFbo;
     if(this.enableMSAA){
       if(this.msaaFbo && screenFbo){
         gl.bindFramebuffer(gl.FRAMEBUFFER, this.msaaFbo.framebuffer);
@@ -245,6 +295,7 @@ class Scene {
     if(this.enableMSAA){
       if(this.msaaFbo && currentFbo !== null && !!screenFbo){
         this.msaaToFbo(this.msaaFbo, screenFbo)
+        this.fboDepth2fbo(this.msaaFbo.framebuffer, this.msaaFbo.attachments[2], screenFbo.framebuffer, screenFbo.attachments[2])
       }
     }
 
